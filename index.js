88884f46dd5baae6c2aeaffb7cefc531e5ad5381
Charles <support@rvtroop.com>
2021-01-28 20:58:09 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+const fs = require('fs');
+const helmet = require('helmet');
+
+app.use(helmet());
+app.disable('x-powered-by');
+
+const port = process.env.PORT || 5000;
+
+// accept all routes
+app.get('*', function (req, res) {
+  res.send('Welcome to RV Troop');
+});
+
+const server = app.listen(port, () => {
+  console.log(`Webapp listening on port ${port} ...`);
+});
+
+module.exports = server;
